86fac01b7447a3677c96dfa7eaececaffdd554ff
tracer-nju <tracer@nju.edu.cn>
2026-04-12 10:57:20 +0800

    Darwin MacBook-Air 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:39 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T8122 arm64 10:57  up 2 days, 20:35, 2 users, load averages: 1.37 1.87 1.99

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -63,7 +63,7 @@ char *resolve_command(const char *command){
 int main(int argc, char *argv[]) {
 
     if(argc<2){
-        fprinf(stderr,"Usage: %s COMMAND [ARG]...\n",argv[0]);
+        fprintf(stderr,"Usage: %s COMMAND [ARG]...\n",argv[0]);
         exit(1);
     }
     //define argments
